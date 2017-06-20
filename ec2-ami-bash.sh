76f8afae87d5c2df269e965f96731d786176975d
kaustubhcs <kaustubhcs07@gmail.com>
2017-06-20 06:58:50 +0530

    Code updated
    ~KTB

--- ec2-ami-bash.sh
+++ ec2-ami-bash.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
+REGION=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone/)
+REGION=${REGION%a}
+REGION=${REGION%b}
+REGION=${REGION%c}
+REGION=${REGION%d}
+REGION=${REGION%e}
+REGION=${REGION%f}
+REGION=${REGION%g}
+REGION=${REGION%h}
+REGION=${REGION%i}
+REGION=${REGION%j}
+REGION=${REGION%k}
+REGION=${REGION%l}
+REGION=${REGION%m}
+REGION=${REGION%n}
+REGION=${REGION%o}
+REGION=${REGION%p}
 yum update -y
-yum install httpd -y
+yum install -y httpd24 php70 mysql56-server php70-mysqlnd
+yum install ruby wget -y
+chmod -R 777 /etc/httpd/
+chmod -R 777 /var/www/html/
+chmod -R 777 /home/ec2-user/
+cd /home/ec2-user
+wget https://aws-codedeploy-$REGION.s3.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
 service httpd start
 chkconfig httpd on
-aws s3 cp s3://mywebsitebucket-acloudguru/index.html /var/www/html
